ce0ee626b9a33282ca55e3fd6e4f315ce6d1f0dd
Mafarein <100634545+Mafarein@users.noreply.github.com>
2023-05-17 00:18:56 +0200

    Update similarity_f

--- main.py
+++ main.py
@@ -62,13 +62,19 @@ def similarity_f(structure, vertices, hic_map):
                          vertices[vertices['id'] == structure[j]]['z']])
             distance_matrix[i, j] = np.sqrt(np.sum((a-b)**2))
 
-    # A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    # vals = np.reshape(A, (9))
-    # pts = np.array([[i, j] for i in [0.0, 0.5, 1.0] for j in [0.0, 0.5, 1.0]])
-    # grid_x, grid_y = np.mgrid[0:1:7j, 0:1:5j]
-    # grid_z = inter.griddata(pts, vals, (grid_x, grid_y), method='linear')
+    vals = np.reshape(distance_matrix, distance_matrix.size)
+    pts = np.array([[i, j] for i in [0.0, 0.5, 1.0] for j in [0.0, 0.5, 1.0]])
+    grid_x, grid_y = np.mgrid[0:1:50j, 0:1:50j] # 50x50
+    grid_z = inter.griddata(pts, vals, (grid_x, grid_y), method='linear')
 
-    return distance_matrix
+    distance_matrix = np.linalg.inv(grid_z)
+
+    corr = np.corrcoef(distance_matrix, hic_map)
+
+    # Zwracamy macierz czy jakąś wartość?
+    # print(corr[1,0])
+
+    return corr
 
 def main():
     edges = pd.read_csv('data/WB_mesh/d8_E.csv')
@@ -79,6 +85,12 @@ def main():
     hic = np.load('data/hic_050.npy')
     print(hic)
     print(similarity_f(structures[1], vertices, hic))
+    
+    # A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # vals = np.reshape(A, (9))
+    # pts = np.array([[i, j] for i in [0.0, 0.5, 1.0] for j in [0.0, 0.5, 1.0]])
+    # grid_x, grid_y = np.mgrid[0:1:7j, 0:1:5j]
+    # grid_z = inter.griddata(pts, vals, (grid_x, grid_y), method='linear')
 
 
 if __name__ == '__main__':
